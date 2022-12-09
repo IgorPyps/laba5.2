4bcfd8f9a5eb81f9870c1bbbd1fe3084f271978f
IgorPyps <115737998+IgorPyps@users.noreply.github.com>
2022-12-09 02:25:05 +0200

    Add files via upload

---
+++ 5.1.1/unit5.1/unit5.1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../5.1.1/5.1.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unit51
+{
+	TEST_CLASS(unit51)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double c = (k(1 + (1 * 1), 1 - (1 * 1)) - (k(1, 1 * 1) * k(1, 1 * 1))) / (1 + k(1 * 1, 1));
+			Assert::AreEqual(c, -1.0385655946774592);
+		}
+	};
+}
